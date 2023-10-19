fbb203b51af0ca414bf6f6b4bc768835691e4ff1
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-10-19 16:51:48 -0300

    adicionando métoo sincronized

---
+++ Synchronized.java
@@ -0,0 +1,35 @@
+public class Synchronized {
+    static int i = -1;
+
+    public static void main(String[] args) {
+        MeuRunnable runnable = new MeuRunnable();
+        String name = Thread.currentThread().getName();
+        System.out.println(name + "Linha principal");
+
+        Thread t0 = new Thread(runnable);
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+        Thread t3 = new Thread(runnable);
+        Thread t4 = new Thread(runnable);
+        
+        t0.start();
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+
+    public static class MeuRunnable implements Runnable {
+        // static Object lock1 = new Object();
+        // static Object lock2 = new Object();
+
+        @Override
+        public void run (){
+            synchronized(this) { //lpck1
+                for (int j = 0; j < 1000; j++, i++);
+            }
+            String name = Thread.currentThread().getName();
+            System.out.println(name + ": " + i);
+        }     
+    }
+}
